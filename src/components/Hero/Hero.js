218329ec7434e9444d79f6427267cef4fa7783e4
Michal S <michalsienczuk@gmail.com>
2021-01-23 19:34:42 +0100

    component column done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
-class Hero extends React.Component {
-    render() {
-        return (
-            <header className={styles.component}>
-                <h2 className={styles.title}>Things to do</h2>
-                <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" />
-            </header>
-        );
-    }
-}
+const Hero = props => (
+    <header className={styles.component}>
+        <h2 className={styles.title}>{props.titleText}</h2>
+        <img className={styles.image} src={props.src}/>
+    </header>
+)
+
+Hero.propTypes = {
+    titleText: PropTypes.node.isRequired,
+    src: PropTypes.string.isRequired,
+};
 
 export default Hero;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from './../Hero/Hero.js';
+import PropTypes from 'prop-types';
+import Column from './../Column/Column.js';
 
 class List extends React.Component {
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        children: PropTypes.node.isRequired,
+        src: PropTypes.string.isRequired
+    }
+
+    static defaultProps = {
+        children: <p>I can do all the things!!!</p>,
+    }
+
     render() {
         return (
             <section className={styles.component}>
-                <Hero />
+                <Hero titleText={this.props.title} src={this.props.src} />
+                {this.props.children}
+                <div className={styles.columns}>
+                    <Column title={['Passion']}/>
+                    <Column title={['Work']}/>
+                    <Column title={['Free Time']}/>
+                </div>
             </section>
         )
     }
