1eea710e143acacf3baaa9855014cd1505a36cb7
Michal S <michalsienczuk@gmail.com>
2021-01-24 19:52:12 +0100

    Card and Icon done - icon needs some corrections

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from './../Card/Card.js';
+import {settings} from '../../data/dataStore';
+import Creator from './../Creator/Creator.js';
+import Icon from './../Icon/Icon.js';
+
 
 class Column extends React.Component {
-    static propTypes = {
-        title: PropTypes.node.isRequired,
+    state = {
+        cards: this.props.cards || [],
+    }
 
+    static propTypes = {
+        title: PropTypes.node,
+        addCard: PropTypes.func,
+        cards: PropTypes.array,
+        icon: PropTypes.string,
+    }
+
+    addCard(title){
+        this.setState(state => (
+            {
+                cards: [
+                    ...state.cards,
+                    {
+                        key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+                        title,
+                        icon: 'list-alt',
+                        cards: []
+                    }
+                ]
+            }
+        ));
     }
 
     render() {
+        const {icon} = this.props;
         return (
             <section className={styles.component}>
-                <h3 className={styles.title}>{this.props.title}></h3>
+                <h3 className={styles.title}>{this.props.title}>
+                    <span className={styles.icon}>
+                        <Icon name={icon}/>
+                    </span>
+                </h3>
+                <div className={styles.card}>
+                    {this.state.cards.map(({key, ...cardProps}) => (
+                        <Card key={key} {...cardProps} />
+                    ))}
+                </div>
+                <div className={styles.creator}>
+                    <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+                </div>
             </section>
         )
     }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -20,21 +20,7 @@ class List extends React.Component {
     static defaultProps = {
         description: settings.defaultListDescription,
     }
-    addColumn(title){
-        this.setState(state => (
-            {
-                columns: [
-                    ...state.columns,
-                    {
-                        key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
-                        title,
-                        icon: 'list-alt',
-                        cards: []
-                    }
-                ]
-            }
-        ));
-    }
+
     render() {
         return (
             <section className={styles.component}>
