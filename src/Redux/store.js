315d936cfc7468240c7078b68c94d23eedd9fc3c
Michal S <michalsienczuk@gmail.com>
2021-02-09 15:57:19 +0100

    test2

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,7 +2,7 @@ import {combineReducers, createStore} from 'redux';
 import initialStoreData from '../data/dataStore';
 import columnsReducer from './columnsRedux';
 import cardsRedux from './cardsRedux';
-import searchStringReducer from './searchStringRedux';
+import searchStringReducer from './searchStringRedux.js';
 
 // define initial state and shallow-merge initial data
 const initialState = {
@@ -10,7 +10,7 @@ const initialState = {
   lists: initialStoreData.lists,
   columns: initialStoreData.columns,
   cards: initialStoreData.cards,
-  searchString: '',
+  searchString: 'ter',
 };
 
 // define reducers
